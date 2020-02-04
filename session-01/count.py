ec4ebf06a27b47cc13af314e1f94a5580fcc7d3b
mconde <m.conde.2019@alumnos.urjc.es>
2020-02-04 16:23:22 +0100

    just another text

---
+++ session-01/count.py
@@ -0,0 +1,3 @@
+#ex 2
+for i in range(1, 21):  i: 4
+    print(i, end=' ')
--- session-01/sum20.py
+++ session-01/sum20.py
@@ -1,15 +1,3 @@
-print("hello")
-print("hello world.  changed")
-
-#ex 1
-print("another print added")
-print("just testing....")
-
-#ex 2
-for i in range(1, 21):  i: 4
-    print(i, end=' ')
-
-
 #ex 3
 #calculate the sum of the 20 first integer numbers
 #1 + 2 + 3 +... + 20
