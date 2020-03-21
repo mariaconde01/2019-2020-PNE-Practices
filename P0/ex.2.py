846642a25f8931109ec0ba1045706dde5465f10b
mariaconde01 <m.conde.2019@alumnos.urjc.es>
2020-03-21 19:46:05 +0100

    changesss

--- P0/ex.2.py
+++ P0/ex.2.py
@@ -1,8 +1,9 @@
 from seq0 import *
 
-FOLDER = "../SESSION-04/"
-FILENAME = "U5.txt"
+folder = "../SESSION-04/"
+filename = "U5.txt"
 
-seq_dna = Seq0.seq_read_fasta(FOLDER + FILENAME)[0:20]
-print("DNA file:", FILENAME)
-print("The first 20 DNA bases are:", seq_dna)
+full_filename =folder + filename
+
+print("DNA file:", filename)
+print("The first 20 bases are", full_name)
--- P0/seq0.py
+++ P0/seq0.py
@@ -7,8 +7,8 @@ def seq_ping():
 def seq_read_fasta(filename):
 
     file_contents = Path(filename).read_text().split("\n")[1:]
-    seq_dna = Path(filename).join(contents)
-    return seq_dna
+    seq = Path(filename).join(contents)
+    return seq
 
 #Read a file with a DNA sequence in FASTA format
 #eliminates the head part to leave only the body part
@@ -19,4 +19,10 @@ def seq_len(seq):
     length=len(seq)
     return(length)
 
+def seq_count_base(seq, base):
+    counter= 0
+    for element in seq:
+        if element == base:
+            count = counter + 1
+    return counter
 
