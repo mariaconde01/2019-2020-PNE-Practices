bddc30168ec2bb330593cf2440764357b10f86a2
mariaconde01 <m.conde.2019@alumnos.urjc.es>
2020-04-01 13:58:25 +0200

    exercises from session 6

--- session-06/Ex4.py
+++ session-06/Ex4.py
@@ -20,9 +20,27 @@ class Seq:
     def len(self):
         return len(self.strbases)
 
-#main program
-def print_seqs(seq_list, colour):
+def print_seqs(seq_list, color):
     index = 0
     for element in seq_list:
-        termcolor.cprint(f"Sequence {index}: (Lenght {element.len()}) {element}")
-        index+=1
+        termcolor.cprint(f"Sequence {index}: (Length: {element.len()}) {element}", color)
+        index += 1
+
+def generate_seqs(pattern, number):
+    list=[]
+    for element in range(1, number+1):
+        sequence=pattern*element
+        list.append(Seq(sequence))
+    return list
+
+#main program
+seq_list1 = generate_seqs("A",3)
+seq_list2 = generate_seqs("AC",5)
+
+termcolor.cprint("List 1:", "blue")
+print_seqs(seq_list1, "blue")
+
+
+print()
+termcolor.cprint("List 2:", "green")
+print_seqs(seq_list2, "green")
