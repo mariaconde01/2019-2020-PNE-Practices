40563b1fbdfb1863b7ec102d5ab3aa6b80c471f4
mariaconde01 <m.conde.2019@alumnos.urjc.es>
2020-04-02 22:17:03 +0200

    exercises from session 7

--- P1/Ex10.py
+++ P1/Ex10.py
@@ -1,13 +1,15 @@
-from seq0 import *
+from Seq1 import *
 
-folder = "../SESSION-04/"
+folder = "../session-04/"
 filename = ".txt"
 
-gene_list =["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
-base_list=["A", "C", "T", "G"]
-print("-----| Exercise 8 |------")
+print("-----|Practice 1, Exercise 10|-----")
 
-for element in gene_list:  #elemento es U5,ADA..
-    dna_seq = seq_read_fasta(folder + element + filename)
-    print( "Gene", element, ":", "Most frequent Base:", processing_genes(dna_seq))
+gene_list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+base_list = ["A", "C", "T", "G"]
 
+for element in gene_list:  # elemento es U5,ADA..
+    dna_file = folder + element + filename
+    s = Seq()
+    s1=Seq(s.read_fasta(dna_file))
+    print("Gene", element, ":", "Most frequent Base:", s1.processing_genes(base_list))
--- P1/Seq1.py
+++ P1/Seq1.py
@@ -1,16 +1,17 @@
 class Seq:
     NULL = "NULL"
     ERROR = "ERROR"
+
     def __init__(self, strbases= "NULL"):
         if strbases == self.NULL:
             self.strbases = self.NULL
             print("NULL sequence created!")
             return
-        good=True
+        good = True
         for i in strbases:
             if i != "A" and i != "C" and i != "G" and i != "T":
                 self.strbases = "ERROR"
-                good=False
+                good = False
                 print("INVALID Seq!")
                 return
 
@@ -24,27 +25,27 @@ class Seq:
         return self.strbases
 
     def len(self):
-        counter=0
+        counter = 0
         if self.strbases == self.NULL:
             return 0
 
-        elif self.strbases==self.ERROR:
+        elif self.strbases == self.ERROR:
             return 0
         else:
-            counter +=1
+            counter += 1
             return len(self.strbases)
 
     def count_base(self, base):
-            counter = 0
-            if self.strbases == self.NULL:
-                return counter
-            elif self.strbases == self.ERROR:
-                return counter
-            else:
-                for character in self.strbases:
-                    if character == base:
-                        counter += 1
-                return counter
+        counter = 0
+        if self.strbases == self.NULL:
+            return counter
+        elif self.strbases == self.ERROR:
+            return counter
+        else:
+            for character in self.strbases:
+                if character == base:
+                    counter += 1
+            return counter
 
     def count(self):
 
@@ -57,12 +58,12 @@ class Seq:
 
     def reverse(self):
 
-       if self.strbases==self.NULL:
-           return("NULL")
-       elif self.strbases == self.ERROR:
-           return("ERROR")
-       else:
-           return self.strbases[::-1]
+        if self.strbases == self.NULL:
+            return ("NULL")
+        elif self.strbases == self.ERROR:
+            return ("ERROR")
+        else:
+            return self.strbases[::-1]
 
     def complement(self):
 
@@ -80,19 +81,14 @@ class Seq:
 
     def read_fasta(self, filename):
         from pathlib import Path
-        string=""
+        string = ""
         file_contents = Path(filename).read_text()
         files = file_contents.split('\n')
-        body=files[1:]
-        string=string.join(body).replace(" ", "")  #pasas el body a string y lo
-        return (string)                            #el valor se tiene que devolver en forma de string
+        body = files[1:]
+        string = string.join(body).replace(" ", "")  # pasas el body a string
+        return (string)  # el valor se tiene que devolver en forma de string
 
-
-
-    def processing_genes(self):
-        dict_base = seq_count(seq)
+    def processing_genes(self, base_list):
+        dict_base = self.count()
         max_base = max(dict_base, key=dict_base.get)  # la funcion get te encuentra la base mas frecuente
         return max_base
-
-
-
