2b5b39fd9b5a766d7d09a38a2f37653bf0f461c5
mariaconde01 <m.conde.2019@alumnos.urjc.es>
2020-04-26 23:05:46 +0200

    exercises from session 13

--- P4/EX5.py
+++ P4/EX5.py
@@ -1,4 +1,5 @@
 import socket
+import termcolor
 from pathlib import Path
 
 IP = "127.0.0.1"
@@ -29,16 +30,19 @@ def GEThtml(path):
 
 
 def process_client(s):
+    # -- Receive the request message
     req_raw = s.recv(2000)
     req = req_raw.decode()
 
     print("Message FROM CLIENT: ")
 
+    # -- Split the request messages into lines
     lines = req.split('\n')
+    # -- The request line is the first
     req_line = lines[0]
 
     print("Request line: ", end="")
-    print(req_line)
+    termcolor.cprint(req_line,"green")
 
     words = req_line.split(' ')
     method = words[0]
@@ -67,20 +71,23 @@ def process_client(s):
     # -- Add the Content-Length
     header += f"Content-Length: {len(resp_body)}\n"
 
+    # -- Build the message by joining together all the parts
     response_msg = status_line + header + "\r\n" + resp_body
     cs.send(response_msg.encode())
 
-#MAIN PROGRAM
-
-#Configure the server
-#Listening socket
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
 
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# -- Optional: This is for avoiding the problem of Port already in use
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+# -- Setup up the socket's IP and PORT
 ls.bind((IP, PORT))
 
+# -- Become a listening socket
 ls.listen()
 
 print("SEQ Server configured!")
